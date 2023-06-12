9d7912428c697a4289efb668fe69d4d715723062
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2023-06-12 15:20:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-import Node from "./Node.js";
 import Tree from "./Tree.js";
-
-import { mergeSort, merge } from "./mergeSort.js";
 import prettyPrint from "./prettyPrint.js";
 
-const removeDuplicate = (arr) => {
-  return Array.from(new Set(arr));
-};
-
-const transArr = (arr) => {
-  return mergeSort(removeDuplicate(arr));
-};
-
-const arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-
-const newArr = transArr(arr);
-
 const newTree = new Tree();
-newTree.buildTree(newArr);
+newTree.buildTree([1, 10, 80, 15, 25, 1]);
+newTree.inserVal(69);
+newTree.inserVal(68);
+newTree.inserVal(67);
+
+newTree.rebalance();
 
 prettyPrint(newTree.root);
